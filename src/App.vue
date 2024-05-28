6dbe3250943b01f6a12ecbeb47d05bf183639d2d
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-28 11:18:46 -0300

    Routes Configuration

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="container-fluid">
       <nav>
         <RouterLink to="/">Home |</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/maps">Maps</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/MapsView.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <h1>maps</h1>
+</template>
