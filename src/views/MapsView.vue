6c4660a6356d5d0ed1eea6c53961d3062ec37481
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-30 15:05:51 -0300

    Country list (without request by name)

--- src/views/MapsView.vue
+++ src/views/MapsView.vue
@@ -7,20 +7,55 @@ export default {
 
   data() {
     return {
-      text: 'default'
+      country: '',
+      countries: []
     }
   },
 
   methods: {
-    handleInput($event) {
-      this.text = $event.target.value
-      console.log(this.text)
+    async handleInput($event) {
+      this.country = ''
+      let country = $event.target.value
+
+      if (country.length > 3) {
+        this.getCountryName(country)
+      }
+    },
+
+    async getCountries() {
+      const response = await fetch('https://restcountries.com/v3.1/all')
+      const data = await response.json()
+      this.countries = data
+    },
+
+    getCountryName(country) {
+      const filteredCountries = this.countries
+        .map((newCountry) => {
+          return newCountry
+        })
+        .filter((c) => {
+          return c.name.common.toLowerCase().includes(country.toLowerCase())
+        })
+
+      this.country = filteredCountries
     }
+  },
+
+  mounted() {
+    this.getCountries()
   }
 }
 </script>
 <template>
   <div class="container p-4">
-    <SearchBar @input="handleInput" :input-text="this.text" />
+    <SearchBar @input="handleInput" :input-text="this.country" />
+    <div v-if="this.country">
+      <ul v-for="c in this.country" :key="c.cioc">
+        <li>
+          <h1 class="hover">{{ c.name.common }}</h1>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+<style scoped></style>
