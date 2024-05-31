2582fc3f60b7eb91a9aed0face6e34906ffc0b09
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-31 09:21:53 -0300

    setup map to options api

--- src/components/GoogleMaps.vue
+++ src/components/GoogleMaps.vue
@@ -2,20 +2,44 @@
   <div ref="map" class="mapContainer"></div>
 </template>
 
-<script setup>
-import { onMounted, ref } from 'vue'
-const map = ref(null)
-const country = {
-  lat: -10,
-  lng: -55
-}
+<script>
+export default {
+  data() {
+    return {
+      country: {
+        lat: -10,
+        lng: -55
+      }
+    }
+  },
 
-onMounted(() => {
-  new window.google.maps.Map(map.value, {
-    center: country,
-    zoom: 4
-  })
-})
+  methods: {
+    async loadMap() {
+      const map = this.$refs.map
+      await new window.google.maps.Map(map, {
+        center: this.country,
+        zoom: 4
+      })
+    }
+  },
+
+  mounted() {
+    this.loadMap()
+  }
+}
+// import { onMounted, ref } from 'vue'
+// const map = ref(null)
+// const country = {
+//   lat: -10,
+//   lng: -55
+// }
+
+// onMounted(() => {
+//   new window.google.maps.Map(map.value, {
+//     center: country,
+//     zoom: 4
+//   })
+// })
 </script>
 
 <style>
